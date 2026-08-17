cf479af05d0a5778a630c79bf41a6ba08d9bf48e
sumit kumar <panditsumit8825@gmail.com>
2026-08-17 18:56:40 +0530

    first changes

--- chapter_01.py
+++ chapter_01.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# Problem no.1
+# print("Twinkle, twinkle, little star, \n" 
+# "How I wonder what you are!\n" 
+# "Up above the world so high,\n" 
+# "Like a diamond in the sky.")
+
+# Problem no. 3
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
+
+# Problem no. 4 and 5
+import os
+
+# Specify the directry you want to list
+directry_path = '/'
+#  List all files and directories in the specified path
+contents = os.listdir(directry_path)
+# Print each file and directry name 
+for item in contents:
+    print(item)
