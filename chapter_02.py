7b52df5e5961a0e5a1a3282478d272164b9dce8f
sumit kumar <panditsumit8825@gmail.com>
2026-08-17 18:59:36 +0530

    first changes

---
+++ chapter_02.py
@@ -0,0 +1,28 @@
+# Problem no. 1
+# a = int(input("Enter a number :"))
+# b = int(input("Enter a number :"))
+# print("sum of two number is :", a+b)
+
+# Problem no. 2
+# a = int(input("Enter a dividend number :"))
+# b = int(input("Enter a divisior number :"))
+# print("Remainder is :", a%b)
+
+# Problem no. 3
+# a = input("Enter a variable to check the type :")
+# print(type(a)) 
+
+# Problem no.4
+# a = int(input("Enter a number :"))
+# b = int(input("Enter a number :"))
+# print("comparision of two variables :",a>b)
+
+# Problem no. 5
+# a = int(input("Enter a number :"))
+# b = int(input("Enter a number :"))
+# print("average of two number is :", (a+b)/2)
+
+# Problem no. 6
+a = int(input("Enter a number :"))
+print("Square of a number is :", a**2)
+print("Square of a number is :", a*a)
