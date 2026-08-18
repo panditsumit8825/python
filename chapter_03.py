06daa1a592bab0345359be882f8b1d047832735a
sumit kumar <panditsumit8825@gmail.com>
2026-08-18 08:32:58 +0530

    first changes

---
+++ chapter_03.py
@@ -0,0 +1,25 @@
+# Problem no. 1
+# name = input("Enter your name :")
+# print("Good Afternoon "+name)
+# print(f"Good Afternoon {name}")
+
+# # Problem no. 2
+# letter = ''' Dear |<name>|,
+#         You are Selected!
+#         <|Date|>'''
+# print(letter.replace("|<name>|","Sumit").replace("<|Date|>","10 Aug 2026"))
+
+# Problem no.3
+# find function return the number of index
+# str = "Kumar  kuamr"
+# str1 = "Kumar345"
+# print(str.find("  ")) # return the positive value of index ,contains double space
+# print(str1.find("  ")) # if find return the -1 it means this string does not contain double space  
+
+# Problem no.4
+# str = "Kumar  345"
+# print(str.replace("  "," "))
+
+# Problem no.5
+letter = "Dear Harry,This Python course is nice. Thanks!"
+print("Dear Harry,\n\tThis Python course is nice.\nThanks!")
---
+++ chapter_04.py
@@ -0,0 +1,11 @@
+# Problen no.1
+# a1=input("Enter a fruit name :")
+# a2=input("Enter a fruit name :")
+# a3=input("Enter a fruit name :")
+# a4=input("Enter a fruit name :")
+# a5=input("Enter a fruit name :")
+# a6=input("Enter a fruit name :")
+# a7=input("Enter a fruit name :")
+
+# store=[a1,a2,a3,a4,a5,a6,a7]
+# print(store)
