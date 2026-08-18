7e75730bb595049917307fba4509cdb090d5e6e2
sumit kumar <panditsumit8825@gmail.com>
2026-08-18 16:49:17 +0530

    chnages

---
+++ chapter_05.py
@@ -0,0 +1,91 @@
+# Problem no.1
+# words={
+#     "aam":"mango",
+#     "santra":"orange",
+#     "kella":"banana"
+# }
+# word=input("Enter a word you want to meaning :")
+# print(words[word])
+
+# Problem no.2
+# unique_number=set()
+# a1=int(input("Enter a number"))
+# unique_number.add(a1)
+# a2=int(input("Enter a number"))
+# unique_number.add(a2)
+# a3=int(input("Enter a number"))
+# unique_number.add(a3)
+# a4=int(input("Enter a number"))
+# unique_number.add(a4)
+# a5=int(input("Enter a number"))
+# unique_number.add(a5)
+# a6=int(input("Enter a number"))
+# unique_number.add(a6)
+# a7=int(input("Enter a number"))
+# unique_number.add(a7)
+# a8=int(input("Enter a number"))
+# unique_number.add(a8)
+# print(unique_number)
+
+# Problem no.3
+# set={18,"18"}
+# print(set)
+# s=set()
+# s.add(18)
+# s.add("18")
+# print(s)
+
+# Problem no.4
+# s=set()
+# s.add(20)
+# s.add(20.0)
+# s.add("20")
+# print(s)
+# print(len(s))
+
+# Problem no.5
+# s={}
+# print(type(s))
+
+# Problem no.6
+# dict={}
+# dict["Anurag"]="Bhojpuri"
+# dict["Vikash"]="Hindi"
+# dict["sheepu"]="English"
+# dict["Abhishek"]="Sanskrit"
+# print(dict)
+
+# Problem no.7
+# dict={}
+# dict["Abhishek"]="Bhojpuri"
+# dict["Sheepu"]="Hindi"
+# dict["Sheepu"]="English"
+# dict["Abhishek"]="Sanskrit"
+# print(dict)
+
+# Problem no.8
+# dict={}
+# dict["Anurag"]="Bhojpuri"
+# dict["Vikash"]="Hindi"
+# dict["sheepu"]="English"
+# dict["Abhishek"]="Bhojpuri"
+# print(dict)
+
+# Problem no.9
+# s={8,7,12,"Harry",[1,2]}
+
+# Problem no.5again
+# d={}
+# name=input("Enter your name :")
+# lang=input("Enter your fav lang :")
+# d.update({name:lang})
+# name2=input("Enter your name :")
+# lang2=input("Enter your fav lang :")
+# d.update({name2:lang2})
+# name3=input("Enter your name :")
+# lang3=input("Enter your fav lang :")
+# d.update({name3:lang3})
+# name4=input("Enter your name :")
+# lang4=input("Enter your fav lang :")
+# d.update({name4:lang4})
+# print(d)
---
+++ chapter_06.py
@@ -0,0 +1,73 @@
+# Problem no.1
+# a=int(input("Enter a number :"))
+# b=int(input("Enter a number :"))
+# c=int(input("Enter a number :"))
+# d=int(input("Enter a number :"))
+# if(a>b&a>c&a>d):
+#     print(f"Greatest number is :",a)
+# elif(b>c&b>d):
+#     print(f"Greatest number is :",b)
+# elif(c>d):
+#     print(f"Greatest number is :",c)
+# else:
+#     print(f"Greatest number is :",d)
+
+# Problem no.2
+# print("give the marks is out of 100 ")
+# sub1=int(input("Enter your sub1 marks :"))
+# sub2=int(input("Enter your sub2 marks :"))
+# sub3=int(input("Enter your sub3 marks :"))
+# if(sub1>=33 and sub2>=33 and sub3>=33 and (sub1+sub2+sub3)/3>=40):
+#     print("You are pass the exam")
+# else:
+#     print("You are fail the exam")
+
+# Problem no.3
+# spam1= "make a lot of money"
+# spam2= "buy now"
+# spam3= "subscribe this"
+# spam4= "click this"
+# message=input("Enter a message :")
+# if((spam1 in message) or (spam2 in message) or (spam3 in message) or(spam4 in message)):
+#     print("It is a spam")
+# else:
+#     print("It is not a spam")
+
+# Problem no.4
+# username=input("Ente your username :")
+# result = len(username)
+# if(result<10):
+#     print("Yes it is less than 10 character")
+# else:
+#     print("No it is not less than 10 character")
+
+# Problem no.5
+# list=["Anurag","raja","vikash""maharaja"]
+# name=input("Enter your name :")
+# if(name in list):
+#     print("Yes my name is in list")
+# else:
+#     print("No my name is in not list")
+
+# Problem no.6
+# marks=int(input("Enter your marks :"))
+# if(marks>90 and marks<=100):
+#     print("Your grade is Ex")
+# elif(marks>80 and marks<=90):
+#     print("Your grade is A")
+# elif(marks>70 and marks<=80):
+#     print("Your grade is B")
+# elif(marks>60 and marks<=70):
+#     print("Your grade is C")
+# elif(marks>50 and marks<=60):
+#     print("Your grade is D")
+# else:
+#     print("Your grade is F")  
+
+
+# Problem no.7
+# post=("Harry is a good boy.")
+# if("Harry" in post):
+#     print("Yes ")
+# else:
+#     print("No")
