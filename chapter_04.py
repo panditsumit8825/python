9872d26355ac037d21fb908a3d25cc79720437ee
sumit kumar <panditsumit8825@gmail.com>
2026-08-18 10:39:35 +0530

    changes

--- chapter_04.py
+++ chapter_04.py
@@ -9,3 +9,42 @@
 
 # store=[a1,a2,a3,a4,a5,a6,a7]
 # print(store)
+
+# Problem no.2
+# a1=int(input("Enter your marks :"))
+# a2=int(input("Enter your marks :"))
+# a3=int(input("Enter your marks :"))
+# a4=int(input("Enter your marks :"))
+# a5=int(input("Enter your marks :"))
+# a6=int(input("Enter your marks :"))
+# a7=int(input("Enter your marks :"))
+
+# store=[a1,a2,a3,a4,a5,a6,a7]
+# # marks =store.sort()
+# print(store)
+# nums=store
+# nums.sort()
+# print(nums)
+
+# Problem no.3
+# tuples is immutable or,not changable
+# tuple1=(3,7,9,"Sumit")
+# print(type(tuple1))
+# tuple1[2]=4
+# print(tuple1)
+
+#here list is mutable or,changable
+# list=[76,7,8,54,10]
+# list[2]=17
+# print(list)
+
+# Problem no.4
+# list=[13,76,38,29]
+# print(list)
+# total_sum=sum(list)
+# print(total_sum)
+
+# Problem no.5
+# number=(7,0,8,0,0,9)
+# count=number.count(0)
+# print(count)
