69e0ba6fd51c216190a5b17ab998f0e948997070
sumit kumar <panditsumit8825@gmail.com>
2026-08-22 15:44:30 +0530

    changes

---
+++ chapter_10.py
@@ -0,0 +1,11 @@
+# Problem no.1
+# class programmer:
+#     company="Microsoft"
+#     def __init__(self,name,salary,id):
+#         self.name=name
+#         self.salary=salary
+#         self.id=id
+# p=programmer("Harry",2000000,205)
+# print(p.company,p.id,p.name,p.salary)
+
+
