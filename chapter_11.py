5c3017c7e3b6469ac388c0158aa198881f762f73
sumit kumar <panditsumit8825@gmail.com>
2026-08-24 20:40:08 +0530

    changes

---
+++ chapter_11.py
@@ -0,0 +1,19 @@
+# Problem no.1
+class twoDvector:
+    def __init__(self,i,j):
+        self.i=i
+        self.j=j
+
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j ")
+
+class threeDvector(twoDvector):
+    def __init__(self,i,j,k):
+        super().__init__(i,j)
+        self.k=k
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j + {self.k}k")
+a=twoDvector(1,2)
+a.show()
+b=threeDvector(1,2,3)
+b.show()
