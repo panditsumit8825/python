a49149b6fc01d17ed58929486c2e44e78ebe30db
sumit kumar <panditsumit8825@gmail.com>
2026-08-26 17:58:09 +0530

    changes

---
+++ python practice/runner_score.py
@@ -0,0 +1,13 @@
+score=[1,3,5,6,6]
+# score.sort()
+# print(score)
+# sc.remove()
+# maxm=max(score)
+# print(maxm)
+# for i in score:
+# unique = set(score)
+# unique.remove(max(unique))
+# second=max(unique)
+# print(second)
+total=sum(score)
+print(total)
---
+++ python practice/scond_grade.py
@@ -0,0 +1,4 @@
+students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+for i in students:
+    print(i)
+    
