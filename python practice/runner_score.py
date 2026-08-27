6fb69ff9ef32008c9e0c6cf14a64328a93ed755b
sumit kumar <panditsumit8825@gmail.com>
2026-08-27 22:54:27 +0530

    changes some

--- python practice/runner_score.py
+++ python practice/runner_score.py
@@ -11,17 +11,3 @@
 # print(second)
 # total=sum(score)
 # print(total)
-def tail_fact(n, acc=1):
-    if n == 0:
-        return acc
-    else:
-        return tail_fact(n-1, acc * n)
-
-def nontail_fact(n):
-    if n == 0:
-        return 1
-    else:
-        return n * nontail_fact(n-1)
-        
-print(tail_fact(5))  
-print(nontail_fact(5))
