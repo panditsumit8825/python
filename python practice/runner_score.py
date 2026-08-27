bbd3d3f13186b5fcbba79512f4342e49021e6c39
sumit kumar <panditsumit8825@gmail.com>
2026-08-27 22:50:22 +0530

    changes

--- python practice/runner_score.py
+++ python practice/runner_score.py
@@ -11,3 +11,10 @@
 # print(second)
 # total=sum(score)
 # print(total)
+def factorial(n):
+    if n == 0:  # Base case
+        return 1
+    else:       # Recursive case
+        return n * factorial(n - 1)
+
+print(factorial(5))
