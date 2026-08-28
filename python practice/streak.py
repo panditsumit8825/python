44766e12a588bf9bb13e2d9bc714ea53a4b0a972
sumit kumar <panditsumit8825@gmail.com>
2026-08-28 23:30:19 +0530

    changes

--- python practice/streak.py
+++ python practice/streak.py
@@ -7,3 +7,10 @@
 # res=recur(10)
 # print(res)
 
+# Practice day -28/08/2026
+
+def display_student(name,age):
+    print(name,age)
+show_student=display_student
+display_student("Harry",35)
+show_student("Harry",27)
