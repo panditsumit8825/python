013c48ab9a1dc77d608f81eb5ef7eae78bbcef0e
sumit kumar <panditsumit8825@gmail.com>
2026-08-28 23:34:24 +0530

    changes

--- python practice/streak.py
+++ python practice/streak.py
@@ -9,8 +9,17 @@
 
 # Practice day -28/08/2026
 
-def display_student(name,age):
-    print(name,age)
-show_student=display_student
-display_student("Harry",35)
-show_student("Harry",27)
+# Renamin function
+# def display_student(name,age):
+#     print(name,age)
+# show_student=display_student
+# display_student("Harry",35)
+# show_student("Harry",27)
+
+# using of the range()
+# print(list(range(4,30,2)))
+
+# largest element in list
+x = [4, 6, 8, 24, 12, 2]
+lagest=max(x)
+print(lagest)
